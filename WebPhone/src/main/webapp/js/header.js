0bc40388329e73df8bd3c528807945f8471abab0
binhan <nguyenduongbinhan.dmx@gmail.com>
2025-09-21 07:26:35 +0700

    feat: todo list

--- WebPhone/src/main/webapp/js/header.js
+++
@@ -1,9 +0,0 @@
-
-var user = document.getElementById("user").getAttribute("value");
-if(user==""){
-	document.getElementById("user").style.display = "none";
-	
-}else{
-	document.getElementById("login").style.display = "none";
-	document.getElementById("user").style.display = "block";
-}
